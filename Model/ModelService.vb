cb10d7c85c2ecde22ab6d061fea47a6ee854a469
sajal <sajal96sin@gmail.com>
2021-04-08 01:14:22 +0530

    https://github.com/sajalyeager/SPE-final-project

---
+++ Model/ModelService.vb
@@ -0,0 +1,261 @@
+Imports System
+Imports System.Threading.Tasks
+Imports System.Collections.Generic
+Imports System.Numerics
+Imports Nethereum.Hex.HexTypes
+Imports Nethereum.ABI.FunctionEncoding.Attributes
+Imports Nethereum.Web3
+Imports Nethereum.RPC.Eth.DTOs
+Imports Nethereum.Contracts.CQS
+Imports Nethereum.Contracts.ContractHandlers
+Imports Nethereum.Contracts
+Imports System.Threading
+Imports SPEFinalProject.Contracts.Model.ContractDefinition
+Namespace SPEFinalProject.Contracts.Model
+
+
+    Public Partial Class ModelService
+    
+    
+        Public Shared Function DeployContractAndWaitForReceiptAsync(ByVal web3 As Nethereum.Web3.Web3, ByVal modelDeployment As ModelDeployment, ByVal Optional cancellationTokenSource As CancellationTokenSource = Nothing) As Task(Of TransactionReceipt)
+        
+            Return web3.Eth.GetContractDeploymentHandler(Of ModelDeployment)().SendRequestAndWaitForReceiptAsync(modelDeployment, cancellationTokenSource)
+        
+        End Function
+         Public Shared Function DeployContractAsync(ByVal web3 As Nethereum.Web3.Web3, ByVal modelDeployment As ModelDeployment) As Task(Of String)
+        
+            Return web3.Eth.GetContractDeploymentHandler(Of ModelDeployment)().SendRequestAsync(modelDeployment)
+        
+        End Function
+        Public Shared Async Function DeployContractAndGetServiceAsync(ByVal web3 As Nethereum.Web3.Web3, ByVal modelDeployment As ModelDeployment, ByVal Optional cancellationTokenSource As CancellationTokenSource = Nothing) As Task(Of ModelService)
+        
+            Dim receipt = Await DeployContractAndWaitForReceiptAsync(web3, modelDeployment, cancellationTokenSource)
+            Return New ModelService(web3, receipt.ContractAddress)
+        
+        End Function
+    
+        Protected Property Web3 As Nethereum.Web3.Web3
+        
+        Public Property ContractHandler As ContractHandler
+        
+        Public Sub New(ByVal web3 As Nethereum.Web3.Web3, ByVal contractAddress As String)
+            Web3 = web3
+            ContractHandler = web3.Eth.GetContractHandler(contractAddress)
+        End Sub
+    
+        Public Function ServiceProvidersQueryAsync(ByVal serviceProvidersFunction As ServiceProvidersFunction, ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of ServiceProvidersOutputDTO)
+        
+            Return ContractHandler.QueryDeserializingToObjectAsync(Of ServiceProvidersFunction, ServiceProvidersOutputDTO)(serviceProvidersFunction, blockParameter)
+        
+        End Function
+
+        
+        Public Function ServiceProvidersQueryAsync(ByVal [returnValue1] As String, ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of ServiceProvidersOutputDTO)
+        
+            Dim serviceProvidersFunction = New ServiceProvidersFunction()
+                serviceProvidersFunction.ReturnValue1 = [returnValue1]
+            
+            Return ContractHandler.QueryDeserializingToObjectAsync(Of ServiceProvidersFunction, ServiceProvidersOutputDTO)(serviceProvidersFunction, blockParameter)
+        
+        End Function
+
+
+        Public Function UsersQueryAsync(ByVal usersFunction As UsersFunction, ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of UsersOutputDTO)
+        
+            Return ContractHandler.QueryDeserializingToObjectAsync(Of UsersFunction, UsersOutputDTO)(usersFunction, blockParameter)
+        
+        End Function
+
+        
+        Public Function UsersQueryAsync(ByVal [returnValue1] As String, ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of UsersOutputDTO)
+        
+            Dim usersFunction = New UsersFunction()
+                usersFunction.ReturnValue1 = [returnValue1]
+            
+            Return ContractHandler.QueryDeserializingToObjectAsync(Of UsersFunction, UsersOutputDTO)(usersFunction, blockParameter)
+        
+        End Function
+
+
+        Public Function AddServiceProviderRequestAsync(ByVal addServiceProviderFunction As AddServiceProviderFunction) As Task(Of String)
+                    
+            Return ContractHandler.SendRequestAsync(Of AddServiceProviderFunction)(addServiceProviderFunction)
+        
+        End Function
+
+        Public Function AddServiceProviderRequestAndWaitForReceiptAsync(ByVal addServiceProviderFunction As AddServiceProviderFunction, ByVal Optional cancellationToken As CancellationTokenSource = Nothing) As Task(Of TransactionReceipt)
+        
+            Return ContractHandler.SendRequestAndWaitForReceiptAsync(Of AddServiceProviderFunction)(addServiceProviderFunction, cancellationToken)
+        
+        End Function
+
+        
+        Public Function AddServiceProviderRequestAsync(ByVal [service_type] As BigInteger, ByVal [name] As String, ByVal [location] As String, ByVal [service] As String, ByVal [email] As String, ByVal [phone] As String, ByVal [charges] As BigInteger) As Task(Of String)
+        
+            Dim addServiceProviderFunction = New AddServiceProviderFunction()
+                addServiceProviderFunction.Service_type = [service_type]
+                addServiceProviderFunction.Name = [name]
+                addServiceProviderFunction.Location = [location]
+                addServiceProviderFunction.Service = [service]
+                addServiceProviderFunction.Email = [email]
+                addServiceProviderFunction.Phone = [phone]
+                addServiceProviderFunction.Charges = [charges]
+            
+            Return ContractHandler.SendRequestAsync(Of AddServiceProviderFunction)(addServiceProviderFunction)
+        
+        End Function
+
+        
+        Public Function AddServiceProviderRequestAndWaitForReceiptAsync(ByVal [service_type] As BigInteger, ByVal [name] As String, ByVal [location] As String, ByVal [service] As String, ByVal [email] As String, ByVal [phone] As String, ByVal [charges] As BigInteger, ByVal Optional cancellationToken As CancellationTokenSource = Nothing) As Task(Of TransactionReceipt)
+        
+            Dim addServiceProviderFunction = New AddServiceProviderFunction()
+                addServiceProviderFunction.Service_type = [service_type]
+                addServiceProviderFunction.Name = [name]
+                addServiceProviderFunction.Location = [location]
+                addServiceProviderFunction.Service = [service]
+                addServiceProviderFunction.Email = [email]
+                addServiceProviderFunction.Phone = [phone]
+                addServiceProviderFunction.Charges = [charges]
+            
+            Return ContractHandler.SendRequestAndWaitForReceiptAsync(Of AddServiceProviderFunction)(addServiceProviderFunction, cancellationToken)
+        
+        End Function
+        Public Function AddUserRequestAsync(ByVal addUserFunction As AddUserFunction) As Task(Of String)
+                    
+            Return ContractHandler.SendRequestAsync(Of AddUserFunction)(addUserFunction)
+        
+        End Function
+
+        Public Function AddUserRequestAndWaitForReceiptAsync(ByVal addUserFunction As AddUserFunction, ByVal Optional cancellationToken As CancellationTokenSource = Nothing) As Task(Of TransactionReceipt)
+        
+            Return ContractHandler.SendRequestAndWaitForReceiptAsync(Of AddUserFunction)(addUserFunction, cancellationToken)
+        
+        End Function
+
+        
+        Public Function AddUserRequestAsync(ByVal [name] As String, ByVal [location] As String, ByVal [email] As String, ByVal [phone] As String) As Task(Of String)
+        
+            Dim addUserFunction = New AddUserFunction()
+                addUserFunction.Name = [name]
+                addUserFunction.Location = [location]
+                addUserFunction.Email = [email]
+                addUserFunction.Phone = [phone]
+            
+            Return ContractHandler.SendRequestAsync(Of AddUserFunction)(addUserFunction)
+        
+        End Function
+
+        
+        Public Function AddUserRequestAndWaitForReceiptAsync(ByVal [name] As String, ByVal [location] As String, ByVal [email] As String, ByVal [phone] As String, ByVal Optional cancellationToken As CancellationTokenSource = Nothing) As Task(Of TransactionReceipt)
+        
+            Dim addUserFunction = New AddUserFunction()
+                addUserFunction.Name = [name]
+                addUserFunction.Location = [location]
+                addUserFunction.Email = [email]
+                addUserFunction.Phone = [phone]
+            
+            Return ContractHandler.SendRequestAndWaitForReceiptAsync(Of AddUserFunction)(addUserFunction, cancellationToken)
+        
+        End Function
+        Public Function GetAllServiceProvidersQueryAsync(ByVal getAllServiceProvidersFunction As GetAllServiceProvidersFunction, ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of List(Of String))
+        
+            Return ContractHandler.QueryAsync(Of GetAllServiceProvidersFunction, List(Of String))(getAllServiceProvidersFunction, blockParameter)
+        
+        End Function
+
+        
+        Public Function GetAllServiceProvidersQueryAsync(ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of List(Of String))
+        
+            return ContractHandler.QueryAsync(Of GetAllServiceProvidersFunction, List(Of String))(Nothing, blockParameter)
+        
+        End Function
+
+
+
+        Public Function GetAllUsersQueryAsync(ByVal getAllUsersFunction As GetAllUsersFunction, ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of List(Of String))
+        
+            Return ContractHandler.QueryAsync(Of GetAllUsersFunction, List(Of String))(getAllUsersFunction, blockParameter)
+        
+        End Function
+
+        
+        Public Function GetAllUsersQueryAsync(ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of List(Of String))
+        
+            return ContractHandler.QueryAsync(Of GetAllUsersFunction, List(Of String))(Nothing, blockParameter)
+        
+        End Function
+
+
+
+        Public Function Service_providersQueryAsync(ByVal service_providersFunction As Service_providersFunction, ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of String)
+        
+            Return ContractHandler.QueryAsync(Of Service_providersFunction, String)(service_providersFunction, blockParameter)
+        
+        End Function
+
+        
+        Public Function Service_providersQueryAsync(ByVal [returnValue1] As BigInteger, ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of String)
+        
+            Dim service_providersFunction = New Service_providersFunction()
+                service_providersFunction.ReturnValue1 = [returnValue1]
+            
+            Return ContractHandler.QueryAsync(Of Service_providersFunction, String)(service_providersFunction, blockParameter)
+        
+        End Function
+
+
+        Public Function Sp_AddressesQueryAsync(ByVal sp_AddressesFunction As Sp_AddressesFunction, ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of String)
+        
+            Return ContractHandler.QueryAsync(Of Sp_AddressesFunction, String)(sp_AddressesFunction, blockParameter)
+        
+        End Function
+
+        
+        Public Function Sp_AddressesQueryAsync(ByVal [returnValue1] As BigInteger, ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of String)
+        
+            Dim sp_AddressesFunction = New Sp_AddressesFunction()
+                sp_AddressesFunction.ReturnValue1 = [returnValue1]
+            
+            Return ContractHandler.QueryAsync(Of Sp_AddressesFunction, String)(sp_AddressesFunction, blockParameter)
+        
+        End Function
+
+
+        Public Function UserExistQueryAsync(ByVal userExistFunction As UserExistFunction, ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of Boolean)
+        
+            Return ContractHandler.QueryAsync(Of UserExistFunction, Boolean)(userExistFunction, blockParameter)
+        
+        End Function
+
+        
+        Public Function UserExistQueryAsync(ByVal [address] As String, ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of Boolean)
+        
+            Dim userExistFunction = New UserExistFunction()
+                userExistFunction.Address = [address]
+            
+            Return ContractHandler.QueryAsync(Of UserExistFunction, Boolean)(userExistFunction, blockParameter)
+        
+        End Function
+
+
+        Public Function User_AddressesQueryAsync(ByVal user_AddressesFunction As User_AddressesFunction, ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of String)
+        
+            Return ContractHandler.QueryAsync(Of User_AddressesFunction, String)(user_AddressesFunction, blockParameter)
+        
+        End Function
+
+        
+        Public Function User_AddressesQueryAsync(ByVal [returnValue1] As BigInteger, ByVal Optional blockParameter As BlockParameter = Nothing) As Task(Of String)
+        
+            Dim user_AddressesFunction = New User_AddressesFunction()
+                user_AddressesFunction.ReturnValue1 = [returnValue1]
+            
+            Return ContractHandler.QueryAsync(Of User_AddressesFunction, String)(user_AddressesFunction, blockParameter)
+        
+        End Function
+
+
+    
+    End Class
+
+End Namespace
--- contracts/Model.sol
+++ contracts/Model.sol
@@ -1,11 +1,21 @@
-// SPDX-License-Identifier: GPL-3.0
+//SPDX-License-Identifier: GPL-3.
 pragma solidity >=0.7.0 <0.9.0;
 
+
+
 contract Model{
+    
+    address payable public admin;
+    
+    constructor(){
+        admin =payable(msg.sender);
+        
+    }
     // structure for Users
     struct User{
         uint id;
         address payable Address;
+        address payable sp;
         string name;
         string location;
         string email;
@@ -19,20 +29,23 @@ contract Model{
         address payable Address;
         string name;
         string location;
-        string service;
+        address payable [] service_seeker;
         string phone;
         string email;
         uint charges;
+        uint service_type;
+
     }
-
-
+        
+    
+    address payable[] public service_providers;
     address payable[] public  user_Addresses;
     address payable[] public sp_Addresses;
     uint usersId = 0;
     uint serviceProviderId = 0;
     uint usersCount = 0;
     uint serviceProviderCount = 0;
-
+    
     mapping(address => User) public Users;
     mapping(address => Service_Provider) public ServiceProviders;
 
@@ -41,8 +54,9 @@ contract Model{
         usersCount++;
         usersId++;
         address payable Address = payable(msg.sender);
+        address payable sp=payable(0);
         user_Addresses.push(Address);
-        Users[Address] = User(usersId, Address, name, location,email,phone);
+        Users[Address] = User(usersId, Address, sp ,name, location,email,phone);
     }
 
     // Function to return the array of addresses of Users
@@ -58,18 +72,89 @@ contract Model{
         else
             return false;
     }
+    
+           
 
     // Function to add service providers
-    function addServiceProvider(string memory name, string memory location, string memory service, string memory email, string memory phone, uint  charges ) public payable{
+    function addServiceProvider(string memory name, string memory location,  string memory email, string memory phone, uint  charges ,uint service_type) public payable{
         serviceProviderCount++;
         serviceProviderId++;
         address payable Address = payable(msg.sender);
         sp_Addresses.push(Address);
-        ServiceProviders[Address] = Service_Provider(serviceProviderId, Address, name, location,service, email,phone, charges);
-    }
+        address payable [] memory service_seeker;
+        ServiceProviders[Address] = Service_Provider(serviceProviderId, Address, name, location, service_seeker,email,phone, charges,service_type);
+  
 
+                  
+             
+    }
+         // function to return a service provider list to user
+    function render_List(uint service_type) public{
+        service_providers = new address payable [](0);
+        uint i=0;
+        for (i =0;i<sp_Addresses.length;++i){
+            if(ServiceProviders[sp_Addresses[i]].service_type == service_type){
+
+                service_providers.push(sp_Addresses[i]);}
+        }
+        
+    }
+    
+    function return_rendered_List() public view returns(address payable[] memory){
+        return service_providers;
+    }
     //function to return the array of addresses of Service Providers
     function getAllServiceProviders() public view returns (address payable [] memory){
         return sp_Addresses;
     }
+    
+    //require(msg.value == 2 ether)
+    // function for checking ether value and registeration of user to particular service Providers
+    function requestService(address payable  sp)public payable{
+        address payable user;
+    
+          user = payable ( msg.sender);
+          
+        if(msg.value!=ServiceProviders[sp].charges){
+            user.transfer( msg.value);
+            
+        }
+        else{
+            Users[user].sp=sp;
+            ServiceProviders[sp].service_seeker.push(user);
+        
+        }
+        
+     }
+     
+     function servicecompleted() public payable{
+         address payable user;
+         user= payable(msg.sender);
+         uint i=0;
+         uint length=ServiceProviders[Users[user].sp].service_seeker.length;
+         // loop to check whether user is present in service provider list or not
+         for(;i<length;i++)
+             {
+                 if(ServiceProviders[Users[user].sp].service_seeker[i]==user)
+                     break;
+             }
+        // if user not present then we transfer 0 to the service provider
+        if(i==ServiceProviders[Users[user].sp].service_seeker.length)
+                  Users[user].sp.transfer(0);
+        else{ 
+                 uint amount=(100*ServiceProviders[Users[user].sp].charges-95*ServiceProviders[Users[user].sp].charges)/100;
+                 admin.transfer(amount);
+                 Users[user].sp.transfer((90*ServiceProviders[Users[user].sp].charges)/100);
+                 // this to delete the user from service provider list
+                 ServiceProviders[Users[user].sp].service_seeker[i]=ServiceProviders[Users[user].sp].service_seeker[length-1];
+                 ServiceProviders[Users[user].sp].service_seeker[length-1]=user;
+                 ServiceProviders[Users[user].sp].service_seeker.pop();
+             }
+         
+    }  
+     
+     
+    
+     
+    
 }
